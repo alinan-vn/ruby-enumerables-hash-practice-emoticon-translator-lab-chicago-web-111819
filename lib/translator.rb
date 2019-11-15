befc1939082b969cee027eb2ca0eda05be138663
Alejandro Linan <jandrolinan@gmail.com>
2019-11-15 23:19:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,5 +26,11 @@ end
 
 def get_english_meaning(file_path, emoticon)
   # code goes here
+   emotes = load_library(file_path)
   
+  if emotes["get_emoticon"].include?(emoticon)
+    return emotes["get_emoticon"][emoticon]
+  else 
+    p "Sorry, that emoticon was not found"
+  end
 end
