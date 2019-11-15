b7332fc8e750c19e9aaa6ec0fbc6588ac467fe5f
Alejandro Linan <jandrolinan@gmail.com>
2019-11-15 22:58:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,8 +13,12 @@ def load_library(file_path)
   return result
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
   # code goes here
+  emotes = YAML.load_file(file_path)
+  if emotes.include?(emoticon)
+    return emotes["get_emoticon"][emoticon]
+  end
 end
 
 def get_english_meaning
